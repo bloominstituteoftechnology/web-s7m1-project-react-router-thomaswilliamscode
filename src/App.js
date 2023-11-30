5990bd606ccf325d2e43f337e0c5def7b50afc0e
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2023-11-30 10:40:29 -0500

    MVP complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Routes, Route, Link } from 'react-router-dom'
 import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie';
 
 import SavedList from './Movies/SavedList';
 
@@ -16,6 +17,7 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movies' slice of state
+          setMovies(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -39,8 +41,8 @@ export default function App () {
 			/>
 
 			<Routes>
-				<Route path ='/' element={<MovieList />}/>
-				<Route />
+				<Route path='/' element={<MovieList movies={movies} />} />
+				<Route path='movies/:id' element={<Movie />} />
 			</Routes>
 		</div>
 	);
